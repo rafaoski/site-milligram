3ffa66cbe37c343a538bb36f6df7eac718c7d897
Rafał Olszewicz <rafalolszewicz@gmail.com>
2018-08-25 07:28:27 +0200

    update options

--- templates/_options.php
+++ templates/_options.php
@@ -20,8 +20,11 @@ page()->opt = [
  * 
  */
 'home' => pages()->get('/'), // Get Home Page
+
 'news_p' => pages('/news/'),  // Get News Page
+
 'about_p' => pages('/about/'), // Get About Page
+
 // 'p_url' => pages('/privacy-policy/'), // Privacy Policy
 
 /**
@@ -31,12 +34,16 @@ page()->opt = [
  */
 // Get Favicon
 'favicon_url' => urls()->templates . "dist/img/favicon.png",
+
 // Logo url
 'logo_url' => urls()->templates . "dist/img/logo.png",
+
 // Get main CSS file
 'app_css' => urls()->templates . "dist/app.css",
+
 // Enable Open Graph SEO
 'og_seo' => false,
+
 // Add Google Webmaster Verification Code https://kb.yoast.com/kb/how-to-connect-your-website-to-google-webmaster-tools/
 'verification_code' => '', // Paste Your Code
 
@@ -47,9 +54,11 @@ page()->opt = [
  */ 
 'enable_cf' => false, // Enable contact Form
 'your_mail' => 'yourmail@gmail.com', // Email to send message
+
 // More Info
 'c_phone' => '6755464', // Info Phone
 'c_mail' => 'processwire@gmail.com', // Info E-Mail
+
 // Save Message
 'save_message' => false, // Save mesage to pages
 'c_parent' => 'contact', // Contact Page
@@ -68,13 +77,14 @@ page()->opt = [
  'youtube' => 'https://www.youtube.com/user/ryancramerdesign/videos',
  'github' => 'https://github.com/processwire/processwire'
 ],
+
 // Add Google Fonts
 'g_fonts' => ['Roboto','Montserrat','Righteous'],
+
 // Privacy Banner // https://cookieconsent.insites.com/
 'p_b' => false, // Enable Privacy Banner
 'p_url' => pages()->get('/')->httpUrl . 'privacy-policy/', // Cookie Page ( You Must Create Privacy Policy page )
-// Or    
-   // 'p_url' => pages('/privacy-policy/')->url, // Same as pages()->get('/privacy-policy/')->url
+
 // Google Analytics Code
 'ga_code' => '', // To Enable put Google Analytics Code
 
